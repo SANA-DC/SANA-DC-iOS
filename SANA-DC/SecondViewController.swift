619eeddfd6d504bc1e4c8f132dbf523b6a458696
saleemsangi <greatsangi@yahoo.com>
2017-09-28 12:42:30 -0400

    Initial Commit

---
+++ SANA-DC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SANA-DC
+//
+//  Created by Saleem Sangi on 9/28/17.
+//  Copyright © 2017 Saleem Sangi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
