6d3e8f892c03a317cf63ebbeb95650b3c013c734
saleemsangi <greatsangi@yahoo.com>
2017-11-25 13:40:58 -0500

    Added about me and about sana icon

---
+++ SANA-DC/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutMeViewController.swift
+//  SANA-DC
+//
+//  Created by Saleem Sangi on 11/25/17.
+//  Copyright © 2017 Saleem Sangi. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
