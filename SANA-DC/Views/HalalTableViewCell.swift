42155a34a90ae6d8aa55417f299a303ca522e03a
saleemsangi <greatsangi@yahoo.com>
2018-04-21 20:49:37 -0400

    Added Restaurant Sceen

---
+++ SANA-DC/Views/HalalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HalalTableViewCell.swift
+//  SANA-DC
+//
+//  Created by Saleem Sangi on 4/21/18.
+//  Copyright © 2018 Saleem Sangi. All rights reserved.
+//
+
+import UIKit
+
+class HalalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
